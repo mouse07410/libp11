63d0976df1491ab00e73edc2311e0a3f3b956bbc
Mouse <mouse008@gmail.com>
2017-09-23 23:42:46 -0400

    Add "-g" to CFLAGS and LDFLAGS to simplify debugging

--- libp11-build-1.1.sh
+++ libp11-build-1.1.sh
@@ -6,5 +6,5 @@ unset LDFLAGS
 unset CPPFLAGS
 
 make distclean || true
-autoreconf -iv && OPENSSL_CFLAGS="${CFLAGS} -I${HOME}/openssl-1.1/include" OPENSSL_LIBS="-L${HOME}/openssl-1.1/lib -lssl -lcrypto" LDFLAGS="" CPPFLAGS="" ./configure --prefix=${HOME}/openssl-1.1 --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=${HOME}/openssl-1.1/lib/engines-1.1/ && make clean && make -j 4  all && make check && make install
+autoreconf -iv && OPENSSL_CFLAGS="${CFLAGS} -g -I${HOME}/openssl-1.1/include" OPENSSL_LIBS="-L${HOME}/openssl-1.1/lib -lssl -lcrypto" LDFLAGS="-g" CPPFLAGS="" ./configure --prefix=${HOME}/openssl-1.1 --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=${HOME}/openssl-1.1/lib/engines-1.1/ && make clean && make -j 4  all && make check && make install
 
