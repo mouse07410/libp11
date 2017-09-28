e5ce9efff07e53ea4a0b63bb4722edfa6f6f3c2c
Uri Blumenthal <uri@ll.mit.edu>
2017-09-28 15:50:11 -0400

    Added "-DDEBUG" to compilation

--- libp11-build-1.1.sh
+++ libp11-build-1.1.sh
@@ -6,5 +6,5 @@ unset LDFLAGS
 unset CPPFLAGS
 
 make distclean || true
-autoreconf -iv && OPENSSL_CFLAGS="${CFLAGS} -g -I${HOME}/openssl-1.1/include" OPENSSL_LIBS="-L${HOME}/openssl-1.1/lib -lssl -lcrypto" LDFLAGS="-g" CPPFLAGS="-g" CFLAGS="$CFLAGS -g" ./configure --disable-silent-rules --prefix=${HOME}/openssl-1.1 --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=${HOME}/openssl-1.1/lib/engines-1.1/ && make clean && make -j 2  all && make check && make install
+autoreconf -iv && OPENSSL_CFLAGS="${CFLAGS} -g -I${HOME}/openssl-1.1/include" OPENSSL_LIBS="-L${HOME}/openssl-1.1/lib -lssl -lcrypto" LDFLAGS="-g" CPPFLAGS="-g -DDEBUG" CFLAGS="$CFLAGS -g" ./configure --disable-silent-rules --prefix=${HOME}/openssl-1.1 --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=${HOME}/openssl-1.1/lib/engines-1.1/ && make clean && make -j 2  all && make check && make install
 
