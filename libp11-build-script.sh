8a5f7cc29de76f324c87c73b9a0cc059e06f486f
Mouse <mouse008@gmail.com>
2019-02-17 19:38:06 -0500

    Adding script that build debugging version of libp11 for OpenSSL-1.0.2

---
+++ libp11-build-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+
+make distclean || true
+autoreconf -iv && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" ./configure --disable-silent-rules --prefix=/opt/local --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=/opt/local/lib/engines
+make -j 2 all && make check && sudo make install && sudo chown -R uri *
+
