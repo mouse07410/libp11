80f35fc27d02121440aa5cd76d6aa05d696d9c09
Uri Blumenthal <uri@ll.mit.edu>
2019-06-07 21:52:43 -0400

    Moved auto-build to OpenSSL 1.1.1, deprecating 1.0.2

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+DEBUG=-g
+
 make distclean || true
-autoreconf -iv && ./configure --prefix=/opt/local --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=/opt/local/lib/engines && make clean && make all && make check && sudo make install && sudo chown -R uri *
+autoreconf -iv && CFLAGS="$CFLAGS ${DEBUG}" LDFLAGS="${DEBUG} $LDFLAGS" ./configure --disable-silent-rules --prefix=/opt/local --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=/opt/local/lib/engines-1.1 && make clean && make all && make check && sudo make install && sudo chown -R ur20980 *
 
