a6610a2e1c14bb4f02bc437f522c08c3ac374409
Mouse <mouse008@gmail.com>
2019-06-07 21:50:41 -0400

    Changed the auto-build script to building with OpenSSL_1_1_1 (deprecated 1.0.2)

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+# Updated for building with OpenSSL-1.1.1 (Macports moved to OpenSSL v1.1.1)
+
 make distclean || true
-autoreconf -iv && ./configure --prefix=/opt/local --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=/opt/local/lib/engines && make clean && make all && make check && sudo make install && sudo chown -R uri *
+autoreconf -iv && ./configure --prefix=/opt/local --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=/opt/local/lib/engines-1.1 && make clean && make all && make check && sudo make install && sudo chown -R uri *
 
