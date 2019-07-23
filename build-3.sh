ac0fef29f79a1105a21ff3333358b2f7b40452e2
Uri Blumenthal <uri@ll.mit.edu>
2019-07-23 17:25:02 -0400

    Renaming OpenSSL master-related from 1.1 to 3

---
+++ build-3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+unset OPENSSL_CFLAGS
+unset OPENSSL_LIBS
+unset LDFLAGS
+unset CPPFLAGS
+
+DEBUG="-g"
+
+make distclean || true
+
+# Build libp11 flexible, using URI via p11-kit
+
+# Build libp11 fixed for OpenSC PKCS#11 library (seems necessary for OpenSSL-3.0 master)
+autoreconf -iv && OPENSSL_CFLAGS="${CFLAGS} ${DEBUG} -I${HOME}/openssl-3/include" OPENSSL_LIBS="-L${HOME}/openssl-3/lib -lssl -lcrypto" LDFLAGS="-g" CPPFLAGS="${DEBUG}" ./configure --disable-silent-rules --prefix=${HOME}/openssl-3 --with-pkcs11-module=/Library/OpenSC/lib/opensc-pkcs11.dylib --with-enginesdir=${HOME}/openssl-3/lib/engines-3/ && make clean && make -j 2  all && make check && make install
+
+#
