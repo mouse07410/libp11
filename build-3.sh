47617f307dfef987abec86aafddabb360f66dd6b
Uri Blumenthal <uri@ll.mit.edu>
2019-08-14 10:52:04 -0400

    Fixed a typo that missed "|tee" for check output

--- build-3.sh
+++ build-3.sh
@@ -21,6 +21,6 @@ make distclean || true
 
 # Build libp11 fixed for OpenSC PKCS#11 library (seems necessary for OpenSSL-3.0 master)
 autoreconf -ivf 
-./configure --disable-silent-rules --prefix=${OPENSSL_DIR} --with-pkcs11-module=/Library/OpenSC/lib/opensc-pkcs11.dylib --with-enginesdir="${OPENSSL_DIR}/lib/engines-3/"  2>&1 | tee conf-3-out.txt && make clean && make -j 2  all 2>&1 | tee make-3-out.txt && make check 2>&1 check-3-out.txt && make install
+./configure --disable-silent-rules --prefix=${OPENSSL_DIR} --with-pkcs11-module=/Library/OpenSC/lib/opensc-pkcs11.dylib --with-enginesdir="${OPENSSL_DIR}/lib/engines-3/"  2>&1 | tee conf-3-out.txt && make clean && make -j 2  all 2>&1 | tee make-3-out.txt && make check 2>&1 | tee check-3-out.txt && make install
 
 #
