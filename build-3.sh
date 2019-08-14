418175af8f3b3682fd12bd7ab14698357ebfdfbb
Uri Blumenthal <uri@ll.mit.edu>
2019-08-14 10:47:16 -0400

    Modified build-3.sh to make it more parametrized

--- build-3.sh
+++ build-3.sh
@@ -7,12 +7,20 @@ unset CPPFLAGS
 
 DEBUG="-g"
 
+export OPENSSL_DIR="$HOME/openssl-3"
+
+export OPENSSL_CFLAGS="${CFLAGS} ${DEBUG} -I${OPENSSL_DIR}/include"
+export OPENSSL_LIBS="-L${OPENSSL_DIR}/lib -lssl -lcrypto"
+export LDFLAGS="${DEBUG} "
+export CPPFLAGS="${DEBUG} "
+export PKG_CONFIG_PATH="${OPENSSL_DIR}/lib/pkgconfig:${PKG_CONFIG_PATH}"
+
 make distclean || true
 
 # Build libp11 flexible, using URI via p11-kit
 
 # Build libp11 fixed for OpenSC PKCS#11 library (seems necessary for OpenSSL-3.0 master)
 autoreconf -ivf 
-OPENSSL_CFLAGS="${CFLAGS} ${DEBUG} -I${HOME}/openssl-3/include" OPENSSL_LIBS="-L${HOME}/openssl-3/lib -lssl -lcrypto" LDFLAGS="-g" CPPFLAGS="${DEBUG}" ./configure --disable-silent-rules --prefix=${HOME}/openssl-3 --with-pkcs11-module=/Library/OpenSC/lib/opensc-pkcs11.dylib --with-enginesdir="${HOME}/openssl-3/lib/engines-3/"  2>&1 | tee conf-3-out.txt && make clean && make -j 2  all 2>&1 | tee make-3-out.txt && make check 2>&1 check-3-out.txt && make install
+./configure --disable-silent-rules --prefix=${OPENSSL_DIR} --with-pkcs11-module=/Library/OpenSC/lib/opensc-pkcs11.dylib --with-enginesdir="${OPENSSL_DIR}/lib/engines-3/"  2>&1 | tee conf-3-out.txt && make clean && make -j 2  all 2>&1 | tee make-3-out.txt && make check 2>&1 check-3-out.txt && make install
 
 #
