05909fa356273934ff2fb2e15806f40fff4be990
Mouse <mouse008@gmail.com>
2019-08-14 20:21:17 -0400

    Improved build script for OpenSSL-3 (master branch)

--- build-3.sh
+++ build-3.sh
@@ -7,7 +7,12 @@ unset CPPFLAGS
 
 DEBUG="-g"
 
-export OPENSSL_DIR="$HOME/openssl-3"
+if [ "z${OPENSSL_DIR}" = "z" ]; then
+  OPENSSL_DIR="$HOME/openssl-3"
+fi
+if [ "z${ENGINESDIR}" = "z" ]; then
+  ENGINESDIR="${OPENSSL_DIR}/lib/engines-3"
+fi
 
 export OPENSSL_CFLAGS="${CFLAGS} ${DEBUG} -I${OPENSSL_DIR}/include"
 export OPENSSL_LIBS="-L${OPENSSL_DIR}/lib -lssl -lcrypto"
@@ -21,6 +26,6 @@ make distclean || true
 
 # Build libp11 fixed for OpenSC PKCS#11 library (seems necessary for OpenSSL-3.0 master)
 autoreconf -ivf 
-./configure --disable-silent-rules --prefix=${OPENSSL_DIR} --with-pkcs11-module=/Library/OpenSC/lib/opensc-pkcs11.dylib --with-enginesdir="${OPENSSL_DIR}/lib/engines-3/"  2>&1 | tee conf-3-out.txt && make clean && make -j 2  all 2>&1 | tee make-3-out.txt && make check 2>&1 | tee check-3-out.txt && make install
+./configure --disable-silent-rules --prefix=${OPENSSL_DIR} --with-pkcs11-module=/Library/OpenSC/lib/opensc-pkcs11.dylib --with-enginesdir="${ENGINESDIR}/"  2>&1 | tee conf-3-out.txt && make clean && make -j 2  all 2>&1 | tee make-3-out.txt && make check 2>&1 | tee check-3-out.txt && make install
 
 #
