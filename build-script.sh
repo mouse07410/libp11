62a6761ef72e1d601ae995435b3ce280a5cb73f1
Mouse <mouse008@gmail.com>
2019-12-15 19:33:29 -0500

    Adjust the build script for OpenSSL-1.1.1 to differentiate between CI build and local install

--- build-script.sh
+++ build-script.sh
@@ -5,5 +5,9 @@
 make distclean || true
 
 # Updated for building with OpenSSL-1.1.1 (Macports moved to OpenSSL v1.1.1)
-autoreconf -iv && CFLAGS="$CFLAGS ${DEBUG}" LDFLAGS="${DEBUG} $LDFLAGS" ./configure --disable-silent-rules --prefix=/opt/local --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=/opt/local/lib/engines-1.1 && make clean && make all && make check && sudo make install && sudo chown -R uri * && sudo codesign -s "Mac Developer: uri@mit.edu (7TWWJNH7TG)" /opt/local/lib/engines-1.1/pkcs11.dylib
+autoreconf -iv && CFLAGS="$CFLAGS ${DEBUG}" LDFLAGS="${DEBUG} $LDFLAGS" ./configure --disable-silent-rules --prefix=/opt/local --with-pkcs11-module=/opt/local/lib/p11-kit-proxy.dylib --with-enginesdir=/opt/local/lib/engines-1.1 && make clean && make all && make check && sudo make install 
+sudo chown -R uri * || true
+if [ -z "$CI" ]; then
+	sudo codesign -s "Mac Developer: uri@mit.edu (7TWWJNH7TG)" /opt/local/lib/engines-1.1/pkcs11.dylib
+fi
 
